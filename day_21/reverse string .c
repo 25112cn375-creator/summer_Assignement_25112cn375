ca9b5a23dc9be9b81d7256ef27f11dddfc0fb5d8
Anshika Sharma <25112cn375@glbitm.ac.in>
2026-06-27 11:54:08 +0530

    Create reverse string .c

---
+++ day_21/reverse string .c
@@ -0,0 +1,29 @@
+// Write a program to Reverse a string.
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int length = 0;
+    char temp;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    while (str[length] != '\0') {
+        if (str[length] == '\n') {
+            str[length] = '\0'; 
+            break;
+        }  
+        length++;
+    }
+    int start = 0;
+    int end = length - 1;
+    while (start < end) {
+        temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        start++;
+        end--;
+    }
+    printf("Reversed string: %s\n", str);
+    return 0;
+}
