6bcfa34734822a600e53d46f088aafec70e6ac93
Anshika Sharma <25112cn375@glbitm.ac.in>
2026-06-30 22:52:22 +0530

    Create voting.c

---
+++ day_26/voting.c
@@ -0,0 +1,16 @@
+// Write a program to Create Voting Eligibility System.
+#include <stdio.h>
+
+int main() {
+    int age;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    if (age >= 18)
+        printf("Eligible to vote.\n");
+    else
+        printf("Not eligible to vote.\n");
+
+    return 0;
+}
